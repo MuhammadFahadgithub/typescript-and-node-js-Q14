628bc4a22f82157f1b3a7d03d82975e507ad17a0
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-02-28 16:43:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+//Task 14
+// If you could invite anyone, loving or deceased, to dinner, who would 
+//you invite? Make a list othet includes at least three people you'd 
+//like to invite to dinner. then use your list to print a massage to 
+//each person, inviting them to dinner.
+var guestList = ['Mustufa', 'Ubaid', 'faheem', 'asif'];
+for (var i = 0; i < guestList.length; i++) {
+    console.log('Dear Mr. ' + guestList[i] + ',\nIt is our pleasure to invite you in our party.\n\nThank You!');
+}
---
+++ app.ts
@@ -0,0 +1,9 @@
+//Task 14
+// If you could invite anyone, loving or deceased, to dinner, who would 
+//you invite? Make a list othet includes at least three people you'd 
+//like to invite to dinner. then use your list to print a massage to 
+//each person, inviting them to dinner.
+let guestList: string[]= ['Mustufa', 'Ubaid', 'faheem', 'asif'];
+for(let i=0;i<guestList.length; i++){
+console.log('Dear Mr. ' + guestList[i] + ',\nIt is our pleasure to invite you in our party.\n\nThank You!')
+}
